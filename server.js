07af53612dcdc43c29b8d838e5e4360aeeaba3e7
eousuf <eousuf.abdullah@gmail.com>
2025-08-29 15:26:20 +0600

    api added

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/api/tickets",async(req,res)=>{
 app.get("/api/Users",async(req,res)=>{
     try{
         const pool =await poolPromise;
-        const result= await pool.request().query("SELECT * FROM Users");
+        const result= await pool.request().query("SELECT * FROM users");
         console.log(result);
 
         res.status(200).json({
@@ -49,13 +49,44 @@ app.get("/api/Users",async(req,res)=>{
             message:"Server error,try again",
             error:error.message
         });
+    }
+})
+
+app.post("/api/users",async(req,res)=>{
+    try{
+        const {userid,fullname,email,phone,urole} =req.body;
+        if(!userid || !fullname || !email ||!phone || !urole)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required",
+            });
+        }
+        const pool =await poolPromise;
+        const result= 
+        await pool
+        .request()
+        .input("userid",sql.Int,userid)
+        .input("fullname",sql.VarChar,fullname)
+        .input("email",sql.VarChar,email)
+        .input("phone",sql.VarChar,phone)
+        .input("urole",sql.VarChar,urole)
+        .query("Insert into users (userid, fullname, email, phone, urole) values (@userid, @fullname, @email, @phone, @urole)");
+        res.status(201).json({
+             success: true,
+             message: "User inserted successfully",
+             rowsAffected: result.rowsAffected[0],
+        });
+    }
+    catch(error){
+       res.status(500).json(error.message);
     }
 })
 
 app.get("/api/Schedules",async(req,res)=>{
     try{
         const pool =await poolPromise;
-        const result= await pool.request().query("SELECT * FROM Schedules");
+        const result= await pool.request().query("SELECT * FROM schedules");
         console.log(result);
 
         res.status(200).json({
@@ -72,3 +103,4 @@ app.get("/api/Schedules",async(req,res)=>{
         });
     }
 })
+
